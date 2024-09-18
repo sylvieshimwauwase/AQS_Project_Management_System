e02e6f1d44a3956311e4bdd5a69fe4d3ffb80a9d
sylvieshimwauwase <slvshimwa@gmail.com>
2024-09-18 11:30:22 +0200

    resolving errors

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import Signup from './assets/pages/Signup';
 import ResetPassword from './assets/pages/ResetPassword';
 import VerifyCode from './assets/pages/VerifyCode';
 import CreateNewPassword from './assets/pages/PasswordSetup';
-import Createemailemployee from './assets/pages/createemailemployee';
 import Addmember from './assets/pages/Addmember';
+import Createemailemployee from './assets/pages/Createemailemployee';
 import Navbar from './assets/components/Navbar';
+import Sidebar from './assets/components/Sidebar';
 import Dashboard from './assets/pages/Dashboard';
-import Sidebar from './assets/components/sidebar';
  
 function App() {
 
